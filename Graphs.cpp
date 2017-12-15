eaeaa6128c75fec5a71b5b33b41e42ee4b28656e
Xianrun QU <xqu25@wisc.edu>
2017-12-15 16:40:54 -0600

    Create Graphs.cpp

---
+++ Graphs.cpp
@@ -0,0 +1,19 @@
+/*
+class Graph
+{
+    int V;    // No. of vertices
+ 
+    // Pointer to an array containing adjacency
+    // lists
+    list<int> *adj;   
+public:
+    Graph(int V);  // Constructor
+ 
+    // function to add an edge to graph
+    void addEdge(int v, int w); 
+ 
+    // prints BFS traversal from a given source s
+    void BFS(int s);  
+};*/
+
+//BFS
