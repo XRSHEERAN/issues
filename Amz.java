7030e9cace0a506611d36bc9e3bb419d46a2bd62
Xianrun QU <xqu25@wisc.edu>
2018-01-07 11:21:02 -0600

    Update Amz.java

--- Amz.java
+++ Amz.java
@@ -19,5 +19,52 @@ class Solution {
         return gmax;
     }
     
+    //serialization: Solutiion with recurssive preorder search; Most reasonable searching alg
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        traverse(root, sb);
+        return sb.toString();
+    }
+
+    private void traverse(TreeNode node, StringBuilder sb) {
+        if (node == null) {
+            sb.append("null,");
+        } else {
+            sb.append(node.val+",");
+            traverse(node.left, sb);
+            traverse(node.right,sb);
+        }
+    }
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        Queue<String> nodes = new LinkedList<>();
+        nodes.addAll(Arrays.asList(data.split(",")));
+        return buildTree(nodes);
+    }
+    //use queue to get rid of used strings
+    private TreeNode buildTree(Queue<String> nodes) {
+        String val = nodes.remove();
+        if (val.equals("null")) return null;
+        else {
+            TreeNode node = new TreeNode(Integer.valueOf(val));
+            node.left = buildTree(nodes);
+            node.right = buildTree(nodes);
+            return node;
+        }
+    }
+}
+    
     
 }
