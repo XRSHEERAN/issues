caabf9eb515e8d88bd268ae7280a7bd70384fa7c
Xianrun QU <xqu25@wisc.edu>
2018-09-12 19:59:23 -0500

    Create wdst.java

---
+++ Helper/wdst.java
@@ -0,0 +1,36 @@
+public class HelloWorld{
+
+    private static void wsort(int[] A, int st, int ed){
+        int n=ed-st+1;
+        if(n>1){
+            wsort(A,st,st+n/2-1);
+            wsort(A,st+n/2,ed);
+            strt(A,st,ed);
+        }
+    }
+    private static void strt(int[] A, int st, int ed){
+        int n=ed-st+1;
+        if(n==2){
+            if(A[st]<A[ed]) swap(A,st,ed);
+        }
+        else{
+            for(int i=st;i<st+n/4;i++){
+                swap(A,i+n/4,i+n/2);
+            }
+            strt(A,st,st+n/2-1);
+            strt(A,st+n/2,ed);
+            strt(A,st+n/4,st+3*n/4-1);
+        }
+    }
+    private static void swap(int[] A, int i, int j){
+        int temp=A[i];
+        A[i]=A[j];
+        A[j]=temp;
+    }
+    
+    public static void main(String []args){
+        int[] arr=new int[]{2,1,4,3};
+        wsort(arr,0,arr.length-1);
+        System.out.println("Hello World"+arr[0]+arr[1]+arr[2]+arr[3]);
+    }
+}
